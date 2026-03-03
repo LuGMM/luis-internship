06dc0bee209e963feb85a1f4afc443fe4bd51ea8
LuGMM <martinnezl@gmail.com>
2026-03-03 09:37:25 -0500

    Author page made dynamic

--- src/pages/Author.jsx
+++ src/pages/Author.jsx
@@ -1,10 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import AuthorBanner from "../images/author_banner.jpg";
 import AuthorItems from "../components/author/AuthorItems";
-import { Link } from "react-router-dom";
-import AuthorImage from "../images/author_thumbnail.jpg";
+import { Link, useParams } from "react-router-dom";
+
+import axios from "axios";
+
+import Skeleton from "../components/UI/Skeleton";
 
 const Author = () => {
+  const { id } = useParams();
+  const [authorInfo, setAuthorInfo] = useState({});
+  const [collection, setCollection] = useState([]);
+  const [isFollowing, setIsFollowing] = useState(false);
+
+  const getAuthorInfo = async () => {
+    const { data } = await axios.get(
+      `https://us-central1-nft-cloud-functions.cloudfunctions.net/authors?author=${id}`,
+    );
+    setAuthorInfo(data);
+    setCollection(data.nftCollection);
+  };
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+    getAuthorInfo();
+  }, []);
+
   return (
     <div id="wrapper">
       <div className="no-bottom no-top" id="content">
@@ -24,38 +45,96 @@ const Author = () => {
               <div className="col-md-12">
                 <div className="d_profile de-flex">
                   <div className="de-flex-col">
-                    <div className="profile_avatar">
-                      <img src={AuthorImage} alt="" />
+                    {authorInfo.authorImage ? (
+                      <div className="profile_avatar">
+                        <img src={authorInfo.authorImage} alt="" />
 
-                      <i className="fa fa-check"></i>
-                      <div className="profile_name">
-                        <h4>
-                          Monica Lucas
-                          <span className="profile_username">@monicaaaa</span>
-                          <span id="wallet" className="profile_wallet">
-                            UDHUHWudhwd78wdt7edb32uidbwyuidhg7wUHIFUHWewiqdj87dy7
-                          </span>
-                          <button id="btn_copy" title="Copy Text">
-                            Copy
-                          </button>
-                        </h4>
+                        <i className="fa fa-check"></i>
+                        <div className="profile_name">
+                          <h4>
+                            {authorInfo.authorName}
+                            <span className="profile_username">
+                              @{authorInfo.tag}
+                            </span>
+                            <span id="wallet" className="profile_wallet">
+                              {authorInfo.address}
+                            </span>
+                            <button id="btn_copy" title="Copy Text">
+                              Copy
+                            </button>
+                          </h4>
+                        </div>
                       </div>
-                    </div>
+                    ) : (
+                      <div className="profile_avatar">
+                        <Skeleton
+                          height="150px"
+                          width="150px"
+                          borderRadius="50%"
+                        />
+
+                        <div className="profile_name">
+                          <h4>
+                            <Skeleton height="20px" width="150px" />
+                            <span className="profile_username">
+                              <Skeleton height="16px" width="100px" />
+                            </span>
+                            <span id="wallet" className="profile_wallet">
+                              <Skeleton height="16px" width="120px" />
+                            </span>
+                          </h4>
+                        </div>
+                      </div>
+                    )}
                   </div>
+
                   <div className="profile_follow de-flex">
-                    <div className="de-flex-col">
-                      <div className="profile_follower">573 followers</div>
-                      <Link to="#" className="btn-main">
-                        Follow
-                      </Link>
-                    </div>
+                    {authorInfo.followers ? (
+                      <div className="de-flex-col">
+                        <div className="profile_follower">
+                          {authorInfo.followers + (isFollowing ? 1 : 0)}{" "}
+                          followers
+                        </div>
+                        {isFollowing ? (
+                          <Link
+                            to="#"
+                            className="btn-main"
+                            onClick={() => setIsFollowing(!isFollowing)}
+                          >
+                            Unfollow
+                          </Link>
+                        ) : (
+                          <Link
+                            to="#"
+                            className="btn-main"
+                            onClick={() => setIsFollowing(!isFollowing)}
+                          >
+                            Follow
+                          </Link>
+                        )}
+                      </div>
+                    ) : (
+                      <div className="de-flex-col">
+                        <div className="profile_follower">
+                          <Skeleton height="16px" width="100px" />
+                        </div>
+                        <Skeleton
+                          height="52px"
+                          width="125px"
+                          borderRadius="8px"
+                        />
+                      </div>
+                    )}
                   </div>
                 </div>
               </div>
 
               <div className="col-md-12">
                 <div className="de_tab tab_simple">
-                  <AuthorItems />
+                  <AuthorItems
+                    collection={collection}
+                    AuthorImage={authorInfo.authorImage}
+                  />
                 </div>
               </div>
             </div>
